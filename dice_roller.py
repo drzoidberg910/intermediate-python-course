5bc6e33dc0c383ca40d8f93df5875601c61c2d02
Jan Haberland <janhaberland95@gmail.com>
2020-09-13 23:56:07 +0200

    Ich habe mehrere Optionen Hinzugefügt

--- dice_roller.py
+++ dice_roller.py
@@ -8,7 +8,17 @@ def main():
   for i in range(0, dice_rolls):
       roll = random.randint(1, 6)
       dice_sum = dice_sum + roll
-      print(f'You rolled a {roll}')
+      if roll == 1:
+          print(f'You rolled a {roll}! Schade...')
+      elif roll == 2:
+          print(f'You rolled a {roll}! Du wirst besser!')
+      elif roll == 5:
+               print(f'You rolled a {roll}! Knapp daneben!')
+      elif roll == 6:
+          print(f'You rolled a {roll}! Das ist Spitze!')
+      else:
+          print(f'You rolled a {roll}')
+          
   print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
