57881cdba1705d74092d0d5f3952c2cdf5f535e1
Jan Haberland <janhaberland95@gmail.com>
2020-09-14 00:05:02 +0200

    Möglichkeit für Auswahl der Seiten- und Würfelzahl hinzugefügt

--- dice_roller.py
+++ dice_roller.py
@@ -2,23 +2,20 @@ def main():
 
   import random
 
-  dice_rolls = 2
+  dice_size = int(input('Wie viele Seiten hat dein Würfel?'))
+  dice_rolls = int(input('Wie viele Würfel möchtest du werfen?'))
   dice_sum = 0
 
   for i in range(0, dice_rolls):
-      roll = random.randint(1, 6)
+      roll = random.randint(1, dice_size)
       dice_sum = dice_sum + roll
       if roll == 1:
           print(f'You rolled a {roll}! Schade...')
-      elif roll == 2:
-          print(f'You rolled a {roll}! Du wirst besser!')
-      elif roll == 5:
-               print(f'You rolled a {roll}! Knapp daneben!')
-      elif roll == 6:
+      elif roll == dice_size:
           print(f'You rolled a {roll}! Das ist Spitze!')
       else:
           print(f'You rolled a {roll}')
-          
+
   print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
